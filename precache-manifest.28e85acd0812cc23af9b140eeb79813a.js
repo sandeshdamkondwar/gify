6b5355d0e29d97e120e2e538e8a01a9e57bfd9ca
sandeshdamkondwar <sandeshdamkondwar@users.noreply.github.com>
2020-08-03 20:48:36 +0000

    deploy: 6d3b24636b616bbe52d99ce05cd54a8b7fddf9d2

---
+++ precache-manifest.28e85acd0812cc23af9b140eeb79813a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d1c4d54cb1cd1ae26788a038ecc4409",
+    "url": "/gify/index.html"
+  },
+  {
+    "revision": "b101f9767b3b8a57bd1c",
+    "url": "/gify/static/css/main.e0540cf6.chunk.css"
+  },
+  {
+    "revision": "6f2a66f73a6f5e522fff",
+    "url": "/gify/static/js/0.1e0d49e6.chunk.js"
+  },
+  {
+    "revision": "b7e52f5ba6b48136f9807db3b7437ffb",
+    "url": "/gify/static/js/0.1e0d49e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea0ff1da3f413d5f97f7",
+    "url": "/gify/static/js/3.7fecebf6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/gify/static/js/3.7fecebf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "222bb30b4ea6f8d71602",
+    "url": "/gify/static/js/4.080efeb6.chunk.js"
+  },
+  {
+    "revision": "c7ccfce931cbf908603b",
+    "url": "/gify/static/js/5.95c1ec6d.chunk.js"
+  },
+  {
+    "revision": "5198c0a2e4ebe93d9321",
+    "url": "/gify/static/js/6.c29061df.chunk.js"
+  },
+  {
+    "revision": "b101f9767b3b8a57bd1c",
+    "url": "/gify/static/js/main.6e97820e.chunk.js"
+  },
+  {
+    "revision": "ad05c0b3973fc2abc5e4",
+    "url": "/gify/static/js/runtime-main.e4466244.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gify/precache-manifest.71b801558ea4752157a59f7f8f169b98.js"
+  "/gify/precache-manifest.28e85acd0812cc23af9b140eeb79813a.js"
 );
 
 self.addEventListener('message', (event) => {
